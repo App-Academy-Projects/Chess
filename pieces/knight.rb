fce6b89bbd24789b0d132e3f4f046a902ee0c89c
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-12 01:36:18 +0200

    Move diffs of knight

--- pieces/knight.rb
+++ pieces/knight.rb
@@ -8,4 +8,17 @@ class Knight < Piece
     def symbol
     '♞'.colorize(color)
   end
+
+  protected
+
+  def move_diffs
+    [[-2, -1],
+     [-1, -2],
+     [-2, 1],
+     [-1, 2],
+     [1, -2],
+     [2, -1],
+     [1, 2],
+     [2, 1]]
+  end
 end
