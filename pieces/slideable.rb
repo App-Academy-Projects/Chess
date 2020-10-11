fbb64661b3dcfc08fe575b852673f5deb69337e4
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-12 00:09:44 +0200

    Horizontal and Vertical dirs and diagonals dirs

--- pieces/slideable.rb
+++ pieces/slideable.rb
@@ -1,3 +1,23 @@
+HORIZONTAL_AND_VERTICAL_DIRS = [
+    [-1, 0],
+    [0, -1],
+    [0, 1],
+    [1, 0]
+  ]
+
+  DIAGONAL_DIRS = [
+    [-1, -1],
+    [-1, 1],
+    [1, -1],
+    [1, 1]
+  ]
+
 module Slideable
-    
+    def horizontal_and_vertical_dirs
+        HORIZONTAL_AND_VERTICAL_DIRS
+    end
+
+    def diagonal_dirs
+        DIAGONAL_DIRS
+    end
 end
