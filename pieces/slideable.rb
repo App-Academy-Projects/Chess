564fa7b64966480840be0513a7212b7f75904382
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-12 01:07:13 +0200

    Get the moves for slideable piece

--- pieces/slideable.rb
+++ pieces/slideable.rb
@@ -25,4 +25,32 @@ module Slideable
         # Each Subclass should implement this method!
         raise NotImplementedError
     end
+
+    def moves
+        moves = []
+
+        move_dirs.each do |dx, dy|
+            moves += grow_unblocked_moves_in_dir(dx, dy)
+        end
+    end
+
+    def grow_unblocked_moves_in_dir(dx, dy)
+        moves = []
+        pos_x, pos_y = pos
+
+        while true
+            pos_x, pos_y = pos_x + dx, pos_y + dy
+            pos = [pos_x, pos_y]
+
+            break unless board.valid_pos?(pos)
+
+            if board.empty?(pos)
+                moves << pos
+            elsif board[pos].color != color
+                moves << pos
+                break
+            end
+        end
+        moves
+    end
 end
