48ba4ef15c63c4b0a906f058c26cd7f8d90cb155
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-11 22:33:19 +0200

    Fill the Board with pieces (back and pawns rows)

--- board.rb
+++ board.rb
@@ -1,9 +1,34 @@
+require_relative 'null_piece'
+
 SIZE = 8
+COLORS = [:white, :black]
 
 class Board
     attr_reader :rows
     def initialize
-        @rows = Array.new(SIZE) { Array.new(SIZE, nil) }
-        @sentinel = NullPiece
+        @sentinel = NullPiece.instance
+        @rows = Array.new(SIZE) { Array.new(SIZE, @sentinel) }
+
+        COLORS.each do |color|
+            fill_back_row(color)
+            fill_pawns_row(color)
+        end
+    end
+
+    private
+    attr_reader :sentinel
+
+    def fill_back_row(color)
+        back_pieces = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
+
+        i = color == :white ? 0 : 7
+        back_pieces.each_with_index do |piece, j|
+            piece.new(color, self, [i, j])
+        end
+    end
+
+    def fill_pawns_row(color)
+        i = color == :white ? 1 : 6
+        8.times { |j| Pawn.new(color, self, [i, j]) }
     end
 end
