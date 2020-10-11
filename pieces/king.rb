3843d81701d5c108e2b32cbdec28ba59f6206995
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-12 01:47:37 +0200

    Move diffs of King

--- pieces/king.rb
+++ pieces/king.rb
@@ -8,4 +8,15 @@ class King < Piece
     def symbol
         '♚'.colorize(color)
     end
+
+    def move_diffs
+        [[-1, -1],
+        [1, 1],
+        [-1, 0],
+        [0, -1],
+        [0, 1],
+        [1, 0],
+        [-1, 1],
+        [1, -1]]
+    end
 end
