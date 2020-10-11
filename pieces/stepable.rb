0f1f1c3cff51f5d5ef98f10f3510e3e1702ee61e
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-12 01:23:28 +0200

    Get the moves for stepable piece

--- pieces/stepable.rb
+++ pieces/stepable.rb
@@ -1,3 +1,27 @@
 module Stepable
-    
+    def moves
+        moves = []
+
+        move_diffs.each do |dx, dy|
+            pos_x, pos_y = pos
+            pos = [pos_x + dx, pos_y + dy]
+
+            next unless board.valid_pos?(pos)
+
+            if board.empty?(pos)
+                moves << pos
+            elsif board[pos].color != color
+                moves << pos
+            end
+        end
+        
+        moves
+    end
+
+    private
+
+    def move_diffs
+        # Each Subclass should implement this method!
+        raise NotImplementedError
+    end
 end
