02e5bdca27d18c579b370747f2abcfd0b57e9da9
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-13 00:05:22 +0200

    Build colors at pos method

--- display.rb
+++ display.rb
@@ -2,8 +2,22 @@ require 'colorize'
 require_relative 'cursor'
 
 class Display
+    attr_reader :cursor, :board
     def initialize(board)
         @cursor = Cursor.new([0,0], board)
         @board = board
+    end
+
+    def colors_at(i, j)
+        pos = cursor.cursor_pos
+        if pos == [i, j] && cursor.selected
+            background = :light_green
+        elsif pos == [i, j]
+            background = :light_red
+        elsif (i+j).even?
+            background = :black
+        else
+            background = :white
+        end
     end
 end
