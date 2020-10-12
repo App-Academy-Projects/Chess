5a72c043ceaac153e2dfaf2d0eee42b43fb52879
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-12 02:00:06 +0200

    at_start_row? method

--- pieces/pawn.rb
+++ pieces/pawn.rb
@@ -5,4 +5,14 @@ class Pawn < Piece
     def symbol
         '♟'.colorize(color)
     end
+
+    private
+
+    def at_start_row?
+        if :white == color
+            pos[0] == 6
+        else
+            pos[0] == 1
+        end
+    end
 end
