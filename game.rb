66e98204677062ce53dc9caa40b7081e33c893c7
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-13 15:51:02 +0200

    Hash of Players, instead of two player variables

--- game.rb
+++ game.rb
@@ -1,12 +1,16 @@
 require_relative 'board'
 
 class Game
-    attr_reader :board, :display, :white_player, :black_player, :cur_player
+    attr_reader :board, :display, :players, :cur_player
     def initialize
         @board = Board.new
         @display = Display.new(@board)
-        @white_player = HumanPlayer.new(:white, @display)
-        @black_player = HumanPlayer.new(:black, @display)
-        @cur_player = white_player
+        @players = { 
+            white: HumanPlayer.new(:white, @display),
+            black: HumanPlayer.new(:black, @display)
+        }
+        @cur_player = @players[:white]
     end
+
+
 end
