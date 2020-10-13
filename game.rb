691096fc46fd7714516506583a905ae968cbe584
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-13 15:57:45 +0200

    Play the game, but there're not yet implemented methods

--- game.rb
+++ game.rb
@@ -9,8 +9,21 @@ class Game
             white: HumanPlayer.new(:white, @display),
             black: HumanPlayer.new(:black, @display)
         }
-        @cur_player = @players[:white]
+        @cur_player = :white
     end
 
-
+    def play
+        until board.checkmate?(cur_player)
+            begin
+                start_pos, end_pos = players[cur_player].make_move(board)
+                board.move_piece(cur_player, start_pos, end_pos)
+                swap_turn!
+                notify_players
+            rescue => exception
+                puts exception
+                retry
+            end
+            puts "#{cur_player} is checkmated!"
+        end
+    end
 end
